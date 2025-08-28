ab880923cdbc56da21b55433abfba4f32c3a39f7
tyler <Tylerrileyenright200@gmail.com>
2025-08-28 10:30:05 -0500

    I messed something help me please

--- FileName.cpp
+++ FileName.cpp
@@ -2,6 +2,5 @@
 
 int main()
 {
-	std::cout << "hi there my friends";
-
-}
+	std::cout << "hi there my friends";// vision control
+	std::cout << "good bye my friend I will see you in the next live." // vision control
